c40ee85fe31f1cee095ed4236dfc5007c10cb056
ZhiYan Meng <1446752434@qq.com>
2025-07-30 16:25:05 +0800

    dll for draw frame  IMGUI

---
+++ FFS_FrameGUI/framegui.h
@@ -0,0 +1,33 @@
+
+//防止头文件被多次包含（避免重复定义、编译错误） 等价于#ifndef #define #endif 
+#pragma once
+
+
+// DLL 导出/导入控制
+/*
+当前项目的CMakelist会定义FRAMEGUI_EXPORTS，告诉编译器exportdll
+当其他项目通过find_package()或target_link_libraries()链接这个 DLL 时，不会定义FRAMEGUI_EXPORTS，因此头文件中的FRAMEGUI_API会自动变为__declspec(dllimport)
+*/
+#ifdef FRAMEGUI_EXPORTS
+#define FRAMEGUI_API __declspec(dllexport)
+#else
+#define FRAMEGUI_API __declspec(dllimport)
+#endif
+
+#include "ImGui/imconfig.h"
+#include "ImGui/imgui.h"
+#include "ImGui/imgui_impl_dx11.h"
+#include "ImGui/imgui_impl_win32.h"
+#include "ImGui/imgui_internal.h"
+#include "ImGui/imstb_rectpack.h"
+#include "ImGui/imstb_textedit.h"
+#include "ImGui/imstb_truetype.h"
+
+#include <d3d11.h>
+
+#pragma comment(lib,"d3d11.lib")
+
+
+extern "C" {
+	FRAMEGUI_API int draw_gui();
+}
---
+++ FFS_FrameGUI/readme.md
@@ -0,0 +1,27 @@
+# Dll_IMGUI
+参考链接https://www.cnblogs.com/muko-aoi/articles/18659981
+
+
+
+code link https://github.com/iconmzy/cmakeDLL_example
+
+
+
+
+# final target
+
+![image](img/target.png)
+
+
+
+
+
+#  build
+
+mkdir build
+
+cd build
+
+cmake ..
+
+cmake --build . --config --release
